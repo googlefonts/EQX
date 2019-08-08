dd8ba2c137d627495a63288eef83e633520f68c5
Patrick Altair McDonald <hello@patrickaltair.com>
2019-08-08 11:45:48 -0700

    Working demo of react button The essential part here was adding a path to node_modules in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,4 +2,7 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
   // cssModules: true // Unsure whether css modules required
+  sassLoaderOptions: {
+    includePaths: ["node_modules"]
+  }
 })
