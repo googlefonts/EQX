fa02adc7335b83d4441070779f127c7e38cc7ffb
Patrick Altair McDonald <hello@patrickaltair.com>
2019-08-07 22:31:26 -0700

    Adds styles folder for css and demo for material component

---
+++ frontend/components/Header.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import Button from '@material/react-button'
+import "../styles/main.scss"
+
+const Header = () => (
+  <div>
+    Header
+    <Button
+      raised
+      className='button-alternate'
+      onClick={() => console.log('clicked!')}
+    >
+      Click Me!
+    </Button>
+  </div>
+);
+
+export default Header;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -19,12 +19,7 @@ export default class MyApp extends App {
     return (
       <>
         <Head>
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-            crossOrigin="anonymous"
-          />
+          
         </Head>
 
         <Container>
