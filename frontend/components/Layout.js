04bbb5e0a6f0d225ccc191a69e1a676b92ae6e1b
Patrick Altair McDonald <hello@patrickaltair.com>
2019-08-08 22:39:14 -0700

    Clear out layout file

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,13 +1,7 @@
 import Header from './Header';
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD'
-};
-
 const Layout = props => (
-  <div style={layoutStyle}>
+  <div>
     <Header />
     {props.children}
   </div>
