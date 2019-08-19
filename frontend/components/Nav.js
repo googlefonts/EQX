550b3f55b009cfb22fc75a4b2da1373b69ffa013
Patrick Altair McDonald <hello@patrickaltair.com>
2019-08-19 13:55:02 -0700

    Adds and styles side-drawer nav

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -7,23 +7,47 @@ import Drawer, {
   DrawerTitle,
   DrawerContent,
 } from '@material/react-drawer';
+import MaterialIcon from '@material/react-material-icon';
+import List, {ListItem, ListItemGraphic, ListItemText} from '@material/react-list';
 
 import "../styles/main.scss"
 
 // Unfinished, just sample shows where to place items
 const Nav = () => (
-  <Drawer>
-    <DrawerHeader> {/*defaults to div*/}
-      <DrawerTitle tag='h2'> {/*defaults to h3*/}
-        Drawer title
+  <Drawer className="nav-drawer">
+    <DrawerHeader>
+      <DrawerTitle tag='h2'>
+        Menu
       </DrawerTitle>
-      <DrawerSubtitle> {/*defaults to h6*/}
-        drawer subtitle
-      </DrawerSubtitle>
     </DrawerHeader>
 
     <DrawerContent >
-      {/* Nav list items here */}
+      <List>
+        <ListItem>
+          <ListItemGraphic graphic={<MaterialIcon icon='dashboard'/>} />
+          <ListItemText primaryText='Nav Item'/>
+        </ListItem>
+        <ListItem>
+        <ListItemGraphic graphic={<MaterialIcon icon='folder'/>} />
+          <ListItemText primaryText='Nav Item'/>
+        </ListItem>
+        <ListItem>
+          <ListItemGraphic graphic={<MaterialIcon icon='folder_shared'/>} />
+          <ListItemText primaryText='Nav Item'/>
+        </ListItem>
+        <ListItem>
+          <ListItemGraphic graphic={<MaterialIcon icon='create_new_folder'/>} />
+          <ListItemText primaryText='Nav Item'/>
+        </ListItem>
+        <ListItem>
+          <ListItemGraphic graphic={<MaterialIcon icon='group'/>} />
+          <ListItemText primaryText='Nav Item'/>
+        </ListItem>
+        <ListItem>
+        <ListItemGraphic graphic={<MaterialIcon icon='settings'/>} />
+          <ListItemText primaryText='Nav Item'/>
+        </ListItem>
+      </List>
     </DrawerContent>
   </Drawer>
 );
