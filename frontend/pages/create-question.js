ba79668be0158ea457b98892e64cf65034f4b0c5
Patrick Altair McDonald <hello@patrickaltair.com>
2019-08-21 12:14:45 -0700

    Add create question fields

--- frontend/pages/create-question.js
+++ frontend/pages/create-question.js
@@ -1,9 +1,12 @@
 import Layout from '../components/Layout'
 import EditorImagesImport from '../components/EditorImagesImport'
+import CreateQuestionFields from '../components/CreateQuestionFields'
+
 import "../styles/main.scss"
 
 const Index = props => (
   <Layout>
+  	<CreateQuestionFields/>
     <EditorImagesImport/>
   </Layout>
 );
