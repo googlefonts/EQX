f46df4ae160bf0d8f86200a4ca9de5ff08f2cffb
Patrick Altair McDonald <hello@patrickaltair.com>
2019-08-21 21:21:41 -0700

    Adds rough of import tab

--- frontend/components/EditorImagesImport.js
+++ frontend/components/EditorImagesImport.js
@@ -5,6 +5,7 @@ import MaterialIcon from '@material/react-material-icon';
 import {
   Button
 } from '@material/react-typography';
+import {Cell, Grid, Row} from '@material/react-layout-grid';
 
 // Images Tab
 const ImagesTab = props => (
@@ -21,36 +22,59 @@ const EditorTab = props => (
 
 // Import Tab
 const ImportTab = props => (
-    <p>Import tab</p>
+  <div className="import-tab">
+    <div className="import-html">
+        <input type="file" name="import-html-upload" id="import-html-upload"/>
+        <label htmlFor="import-html-upload" className="import-html-upload-label"><MaterialIcon className="paperclip-icon" icon='attach_file' /><Button>Attach your images here</Button></label>
+      </div>
+      <div className="import-css">
+        <input type="file" name="import-css-upload" id="import-css-upload"/>
+        <label htmlFor="import-css-upload" className="import-css-upload-label"><MaterialIcon className="paperclip-icon" icon='attach_file' /><Button>Attach your images here</Button></label>
+      </div>
+    <div className="import-fonts">
+      <ul>
+        <li>
+          list item
+        </li>
+        <li>
+          list item
+        </li>
+      </ul>
+    </div>
+  </div>
 )
 
 class EditorImagesImport extends React.Component {
-  state = {activeIndex: 1};
+  state = {activeIndex: 2};
 
   handleActiveIndexUpdate = (activeIndex) => this.setState({activeIndex});
 
   render() {
     return (
-      <div className="editor-images-import">
-        <TabBar
-          activeIndex={this.state.activeIndex}
-          handleActiveIndexUpdate={this.handleActiveIndexUpdate}
-          className="editor-images-import-tab-bar"
-        >
-          <Tab>
-            <span className='mdc-tab__text-label'>Editor</span>
-          </Tab>
-          <Tab>
-            <span className='mdc-tab__text-label'>Images</span>
-          </Tab>
-          <Tab>
-            <span className='mdc-tab__text-label'>Import</span>
-          </Tab>
-        </TabBar>
-        {this.state.activeIndex == 0 ? <EditorTab/> : null}
-        {this.state.activeIndex == 1 ? <ImagesTab/> : null}
-        {this.state.activeIndex == 2 ? <ImportTab/> : null}
-      </div>
+      <Grid>
+        <Row>
+          <Cell columns={12}>
+            <TabBar
+              activeIndex={this.state.activeIndex}
+              handleActiveIndexUpdate={this.handleActiveIndexUpdate}
+              className="editor-images-import-tab-bar"
+            >
+              <Tab>
+                <span className='mdc-tab__text-label'>Editor</span>
+              </Tab>
+              <Tab>
+                <span className='mdc-tab__text-label'>Images</span>
+              </Tab>
+              <Tab>
+                <span className='mdc-tab__text-label'>Import</span>
+              </Tab>
+            </TabBar>
+            {this.state.activeIndex == 0 ? <EditorTab/> : null}
+            {this.state.activeIndex == 1 ? <ImagesTab/> : null}
+            {this.state.activeIndex == 2 ? <ImportTab/> : null}
+          </Cell>
+        </Row>
+      </Grid>
     );
   }
 }
