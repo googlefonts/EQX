89bab71ba0ee152e90fb57640446e9dd15c6e8a7
Patrick Altair McDonald <hello@patrickaltair.com>
2019-08-26 15:07:37 -0700

    Adds image and fab

--- frontend/components/AnswerQuestionFields.js
+++ frontend/components/AnswerQuestionFields.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MaterialIcon from '@material/react-material-icon';
 import {Cell, Grid, Row} from '@material/react-layout-grid';
 import { Headline4, Body1, Body2 } from '@material/react-typography';
+import Fab from '@material/react-fab'
 
 import "../styles/main.scss"
 
@@ -16,12 +17,21 @@ class AnswerQuestionFields extends React.Component {
     return (
 
       <Grid>
-        <Row>
+        <Row className="answer-question-fields">
           <Cell desktopColumns={2}></Cell>
-          <Cell columns={8} className="answer-question-fields">
+          <Cell columns={8}>
             <Body1 className="question-number">Question #{this.state.questionNumber}</Body1>
             <Headline4 tag="h1" className="question">{this.state.questionValue}</Headline4>
             <Body1>{this.state.descriptionValue}</Body1>
+          </Cell>
+        </Row>
+        <Row className="answer-question-images-comments">
+          <Cell desktopColumns={6} className="question-image-wrapper">
+            <img className="question-image" src="static/type-example.png" alt=""/>
+            <Fab className="question-image-fab" icon={<MaterialIcon icon="fullscreen"/>}/>
+          </Cell>
+          <Cell desktopColumns={6}>
+            <Body1>Your answer</Body1>
           </Cell>
         </Row>
       </Grid>
