393e3e3e95d18624adda9ad1348f98dcce99bfd2
Patrick Altair McDonald <hello@patrickaltair.com>
2019-08-27 15:37:03 -0700

    Moves comment box into component and give states

--- frontend/components/AnswerQuestionFields.js
+++ frontend/components/AnswerQuestionFields.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import MaterialIcon from '@material/react-material-icon';
 import {Cell, Grid, Row} from '@material/react-layout-grid';
 import { Headline4, Body1, Body2 } from '@material/react-typography';
@@ -6,6 +5,8 @@ import Fab from '@material/react-fab'
 import Button from '@material/react-button';
 import List, {ListItem, ListItemText, ListItemGraphic, ListItemMeta} from '@material/react-list';
 import TextField, {Input} from '@material/react-text-field';
+
+import CommentBox from '../components/CommentBox';
 
 import "../styles/main.scss"
 
@@ -22,7 +23,6 @@ class AnswerQuestionFields extends React.Component {
     questionNumber: '9',
     questionValue: 'Does the middle dot on “Ŀ” align with  the center bar of “R”?',
     descriptionValue: 'Certe, inquam, pertinax non numquam eius modi tempora incidunt, ut ita ruant itaque turbent, ut de voluptate ponit.',
-    commentValue: ''
   };
 
   render() {
@@ -47,57 +47,7 @@ class AnswerQuestionFields extends React.Component {
             <Button className="yes" outlined>Yes</Button>
             <Button className="no" outlined>No</Button>
             <Button className="next" raised icon={<MaterialIcon icon="arrow_forward"/>}>Next Question</Button>
-            <div className="comment-box">
-              <div className="comment-box-header">
-                <Body1 className="number">8</Body1>
-                <div className="header-text">
-                  <Body1>5 comments</Body1> 
-                  <Body1>Created 10/13/2019</Body1>
-                </div>
-              </div>
-              <List twoLine className="comment-list">
-                <ListItem>
-                  <ListItemGraphic graphic={<EbenAvi/>} />
-                  <ListItemText
-                    primaryText='Eben Sorkin'
-                    secondaryText='Certe, inquam, pertinax non numquam eius modi tempora incidunt, ut ita.'
-                     />
-                  <ListItemMeta meta='5:30pm' />
-                </ListItem>
-                <ListItem>
-                  <ListItemGraphic graphic={<QuinnAvi/>} />
-                  <ListItemText
-                    primaryText='Quinn Keaveney'
-                    secondaryText='Certe, inquam, pertinax non numquam eius modi tempora incidunt, ut ita.'
-                    />
-                  <ListItemMeta meta='5:37pm' />
-                </ListItem>
-                <ListItem>
-                  <ListItemGraphic graphic={<EbenAvi/>} />
-                  <ListItemText
-                    primaryText='Eben Sorkin'
-                    secondaryText='Certe, inquam, pertinax non numquam eius modi tempora incidunt, ut ita.'
-                     />
-                  <ListItemMeta meta='7:03pm' />
-                </ListItem>
-                <Fab className="comment-list-fab" icon={<MaterialIcon icon="expand_more"/>}/>
-              </List>
-              <div className="add-comment-wrapper">
-                <TextField
-                  inputType='textarea'
-                  fullWidth
-                  label="Add a comment"
-                >
-                  <Input
-                    value={this.state.commentValue}
-                    onChange={(e) => this.setState({commentValue: e.currentTarget.value})} />
-                </TextField>
-              </div>
-              <div className="post-cancel-wrapper">
-                <Button raised>Post</Button>
-                <Button>Cancel</Button>
-              </div>
-            </div>
+            <CommentBox/>
           </Cell>
         </Row>
       </Grid>
