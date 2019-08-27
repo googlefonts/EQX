19b71a14d8f2db370ca372f01f9d4c23f2d231a9
Patrick Altair McDonald <hello@patrickaltair.com>
2019-08-26 19:19:12 -0700

    Adds comment header and comment list

--- frontend/components/AnswerQuestionFields.js
+++ frontend/components/AnswerQuestionFields.js
@@ -4,8 +4,17 @@ import {Cell, Grid, Row} from '@material/react-layout-grid';
 import { Headline4, Body1, Body2 } from '@material/react-typography';
 import Fab from '@material/react-fab'
 import Button from '@material/react-button';
+import List, {ListItem, ListItemText, ListItemGraphic, ListItemMeta} from '@material/react-list';
 
 import "../styles/main.scss"
+
+const QuinnAvi = props => (
+  <img src="static/quinn-avi.png" alt="avatar"/>
+);
+
+const EbenAvi = props => (
+  <img src="static/eben-avi.jpeg" alt="avatar"/>
+);
 
 class AnswerQuestionFields extends React.Component {
   state = {
@@ -27,15 +36,50 @@ class AnswerQuestionFields extends React.Component {
           </Cell>
         </Row>
         <Row className="answer-question-images-comments">
-          <Cell desktopColumns={6} className="question-image-wrapper">
+          <Cell desktopColumns={8} className="question-image-wrapper">
             <img className="question-image" src="static/type-example.png" alt=""/>
             <Fab className="question-image-fab" icon={<MaterialIcon icon="fullscreen"/>}/>
           </Cell>
-          <Cell desktopColumns={6} className="answer-comments">
+          <Cell desktopColumns={4} className="answer-comments">
             <Body1 className="your-answer">Your answer</Body1>
             <Button className="yes" outlined>Yes</Button>
             <Button className="no" outlined>No</Button>
             <Button className="next" raised icon={<MaterialIcon icon="arrow_forward"/>}>Next Question</Button>
+            <div className="comment-box">
+              <div className="comment-box-header">
+                <Body1 className="number">8</Body1>
+                <div className="header-text">
+                  <Body1>5 comments</Body1> 
+                  <Body1>Created 10/13/2019</Body1>
+                </div>
+              </div>
+              <List twoLine avatarList className="comment-list">
+                <ListItem>
+                  <ListItemGraphic graphic={<EbenAvi/>} />
+                  <ListItemText
+                    primaryText='Eben Sorkin'
+                    secondaryText='Certe, inquam, pertinax non numquam eius modi tempora incidunt, ut ita.'
+                     />
+                  <ListItemMeta meta='5:30pm' />
+                </ListItem>
+                <ListItem>
+                  <ListItemGraphic graphic={<QuinnAvi/>} />
+                  <ListItemText
+                    primaryText='Quinn Keaveney'
+                    secondaryText='Certe, inquam, pertinax non numquam eius modi tempora incidunt, ut ita.'
+                    />
+                  <ListItemMeta meta='5:37pm' />
+                </ListItem>
+                <ListItem>
+                  <ListItemGraphic graphic={<EbenAvi/>} />
+                  <ListItemText
+                    primaryText='Eben Sorkin'
+                    secondaryText='Certe, inquam, pertinax non numquam eius modi tempora incidunt, ut ita.'
+                     />
+                  <ListItemMeta meta='7:03pm' />
+                </ListItem>
+              </List>
+            </div>
           </Cell>
         </Row>
       </Grid>
