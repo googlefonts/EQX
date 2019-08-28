527e11bfe7ad4fea833eb5d76119ba69e54bca41
QuiteQuinn <quinn@quitetype.com>
2019-08-28 09:35:26 -0700

    adding changes to header

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -9,11 +9,21 @@ const Footer = () => (
   <footer>
     <Headline5 className="mdc-typography--black" tag="h1">EQX</Headline5>
     <ul>
-      <li><Body1><Link href="#">Privacy</Link></Body1></li>
-      <li><Body1><Link href="#">Terms</Link></Body1></li>
-      <li><Body1><Link href="#">Contact</Link></Body1></li>
-      <li><Body1><Link href="#">Developers</Link></Body1></li>
-      <li><Body1><Link href="#">About</Link></Body1></li>
+      <li>
+        <Body1><Link href="#"><a>Privacy</a></Link></Body1>
+      </li>
+      <li>
+        <Body1><Link href="#"><a>Terms</a></Link></Body1>
+      </li>
+      <li>
+        <Body1><Link href="#"><a>Contact</a></Link></Body1>
+      </li>
+      <li>
+        <Body1><Link href="#"><a>Developers</a></Link></Body1>
+      </li>
+      <li>
+        <Body1><Link href="#"><a>About</a></Link></Body1>
+      </li>
     </ul>
   </footer>
 );
