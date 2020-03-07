a5b1f9ada3bc5a7ab33cb0267b64073ac73d8429
QuiteQuinn <quinn@quitetype.com>
2020-03-06 16:25:56 -0800

    Simplifying Layout page

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -9,7 +9,6 @@ import defaultPage from "../hocs/defaultPage";
 import Cookies from "js-cookie";
 import SignIn from "../components/SignIn";
 import { Menu, AppBar, Container, Grid, Button, Typography } from '@material-ui/core';
-import theme from '../src/theme';
 
 // const Layout = props => (
 // 	<div className="layout-wrapper" data-has-progress-header={props.headerType}>
@@ -23,48 +22,8 @@ import theme from '../src/theme';
 // );
 
 import { ThemeProvider } from '@material-ui/styles';
-// import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
-// import { purple, blue } from '@material-ui/core/colors';
+import theme from '../src/theme';
 
-// const primary = "#6F1B93";
-
-// let theme = createMuiTheme({
-// 	palette: {
-//    	primary: purple,
-// 		// secondary: blue,
-// 		background: {
-// 			paper2: "rgb(245, 245, 245)",
-// 			footer: "rgb(224, 224, 224)"
-// 		}
-// 	},
-// });
-
-// theme = responsiveFontSizes(theme);
-// theme.overrides = {
-// 	...theme.overrides,
-// 	MuiDrawer:{
-// 		...theme.MuiDrawer,
-// 		paperAnchorDockedRight: {
-// 			...theme.paperAnchorDockedRight,
-// 			borderLeft: "none",
-// 			paddingTop: '64px',
-// 			width: 230
-// 		}
-// 	},
-// 	MuiListItem: {
-// 		...theme.MuiListItem,
-// 		root: {
-// 			...theme.root,
-// 			"&$selected": { 
-// 				background: theme.palette.primary.main,
-// 				color: "white",
-// 				"& .MuiSvgIcon-root": {
-// 					color: "white",
-// 				 },
-// 			}
-// 		}
-// 	},
-// }
 
 class Layout extends React.Component {
 	constructor(props) {
@@ -79,7 +38,7 @@ class Layout extends React.Component {
 		return { pageProps };
 	}
 	render() {
-		// console.log(this.props);
+		console.log(this.props);
 		
 		// let isAuthenticated = false;
 		// if	(Cookies.get("jwt")){
@@ -87,7 +46,6 @@ class Layout extends React.Component {
 		// }
 		// const title = "EQX";
 		// console.log(this.props);
-		console.log(Cookies.get("user"));
 		return (
 			<ThemeProvider theme={theme}>
 				<div className="layout-wrapper">
@@ -96,7 +54,7 @@ class Layout extends React.Component {
 						headerType={this.props.headerType} 
 						progressBar={this.props.progressBar}
 					/>
-					{this.props.isAuthenticated ? (	
+					{	this.props.isAuthenticated ? (	
 						<>
 							{	// Logged In
 								<div>
