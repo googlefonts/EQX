b2018aa3ca620107b22d1f36d02b86ed6c5277bc
QuiteQuinn <quinn@quitetype.com>
2020-03-06 16:21:45 -0800

    Adding Radial Grade Component

---
+++ frontend/components/RadialGrade.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { CircularProgress, Typography, Box} from '@material-ui/core';
+
+//////////////////////////////
+// Radial Grade
+
+class RadialGrade extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <Box p={1} align="right" className="radial-grade">
+        <CircularProgress className="radial-background" variant="static" size={120} thickness={6} value={100} />
+        <CircularProgress variant="static" size={120} thickness={6} value={this.props.grade} />
+        <Box className="radial-info" align="center" >
+          <Typography className="radial-info-grade" color="primary" variant="h4">{this.props.grade}</Typography>
+          <Typography className="radial-info-label" color="primary" variant="body2">Grade</Typography>
+        </Box>
+      </Box>
+    );
+  }
+}
+export default RadialGrade;
