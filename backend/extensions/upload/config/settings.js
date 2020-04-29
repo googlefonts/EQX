fa25d0ad60c33cd90c8347a7ad5c8220dc2604bb
noqcks <benny@noqcks.io>
2020-04-29 17:47:44 +0000

    adding google clud storage config

--- backend/extensions/upload/config/settings.js
+++ backend/extensions/upload/config/settings.js
@@ -1,2 +1,13 @@
-// this is where google-cloud-storage integration for uploads will be configured
-module.exports = {};
+if (process.env.GCS_ENABLED === 'true') {
+  module.exports = {
+    provider: "google-cloud-storage",
+    providerOptions: {
+      serviceAccount: process.env.GCS_SERVICE_ACCOUNT_JSON_STRING,
+      bucketName: process.env.GCS_BUCKET_NAME,
+      bucketLocation: process.env.GCS_BUCKET_LOCATION,
+      baseUrl: 'https://storage.googleapis.com/{bucket-name}'
+    }
+  };
+} else {
+  module.exports = {}
+}
