f95f1dda601b90dbbefa16e1aed8b7650183f675
QuiteQuinn <quinn@quitetype.com>
2020-05-05 14:59:32 -0700

    Set Strapi permissions on run #103

--- backend/config/functions/bootstrap.js
+++ backend/config/functions/bootstrap.js
@@ -10,4 +10,17 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/configurations/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+
+	// const authenticated = await strapi.query('role', 'users-permissions').findOne({ type: 'authenticated' });
+	// authenticated.permissions.forEach(permission => {
+		
+	// 	if (permission.type === 'application'){
+	// 		if (permission.controller === 'organization' && permission.action === 'delete'){ return; }
+	// 		let newPermission = permission;
+	// 		newPermission.enabled = true;
+	// 		strapi.query('permission', 'users-permissions').update( { id: newPermission.id }, newPermission );
+	// 	}
+	// });
+	// return;
+};
