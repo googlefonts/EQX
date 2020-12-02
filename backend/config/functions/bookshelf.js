0a5488f415635db1522cc0620dcb564ee652bf65
QuiteQuinn <quinn@quitetype.com>
2020-12-02 12:49:46 -0800

    Updating to post beta file structure

--- backend/config/functions/bookshelf.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = (bookshelf, connection) => {
-  bookshelf.plugin('bookshelf-uuid');
-};
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
+   host: env('HOST', '0.0.0.0'),
+   port: env.int('PORT', 1337),
    admin: {
-      auth: {
-         secret: env('ADMIN_JWT_SECRET'),
-      },
+     auth: {
+       secret: env('ADMIN_JWT_SECRET', '7e518921dd33789e7bdc736b7aadd908'),
+     },
    },
-});
+ });
+ 
