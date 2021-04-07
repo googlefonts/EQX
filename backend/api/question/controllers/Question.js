a443a13a3004467f94a07721585d7c6befbca75d
QuiteQuinn <quinn@quitetype.com>
2021-04-07 12:02:48 -0700

    Updaing Question model

--- backend/api/question/controllers/Question.js
+++ backend/api/question/controllers/Question.js
@@ -5,4 +5,31 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    
+
+
+   // find: ctx => {
+   //    return strapi.query('question').find(ctx.query, [{
+   //       path: 'answers',
+   //       populate: {
+   //          path: 'users',
+   //       },
+   //    }]);
+   // },
+    
+   // find: ctx => {
+   //    console.log("yep")
+   //    return strapi.query('questions').find(ctx.query, ['answer']);
+   // },
+   // async find(ctx) {
+   //    let entities;
+   //    if (ctx.query._q) {
+   //       entities = await strapi.services.restaurant.search(ctx.query);
+   //    } else {
+   //       entities = await strapi.services.restaurant.find(ctx.query);
+   //    }
+
+   //    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.restaurant }));
+   // },
+};
