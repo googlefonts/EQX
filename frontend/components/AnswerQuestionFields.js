5ebf8a5d2b09212cb5eb1ae7aa2ba2fb5fbd4e37
QuiteQuinn <quinn@quitetype.com>
2021-04-07 12:05:42 -0700

    Connecting comments/answers to User

--- frontend/components/AnswerQuestionFields.js
+++ frontend/components/AnswerQuestionFields.js
@@ -1,43 +1,136 @@
 import MaterialIcon from '@material/react-material-icon';
-import { Box, Grid, Card, Fab, CardMedia, ButtonGroup, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormGroup, Input, InputLabel, Button, Typography } from '@material-ui/core';
+import { List, ListItem, ListItemAvatar, CardActions, Avatar, ListItemText, Divider, Box, Grid, Card, CardHeader, Fab, CardMedia, ButtonGroup, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormGroup, Input, InputLabel, Button, Typography } from '@material-ui/core';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import CommentBox from '../components/CommentBox';
 import getConfig from 'next/config';
+import axios from 'axios';
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 const apiUrl = publicRuntimeConfig.API_URL || 'http://localhost:1337';
-
-const QuinnAvi = props => (
-  <img src="static/img/quinn-avi.png" alt="avatar"/>
-);
-
-const EbenAvi = props => (
-  <img src="static/img/eben-avi.jpeg" alt="avatar"/>
-);
+import Cookies from "js-cookie";
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import Router from 'next/router';
+import AddCommentIcon from '@material-ui/icons/AddComment';
+import PermMediaIcon from '@material-ui/icons/PermMedia';
 
 class AnswerQuestionFields extends React.Component {
-
-  state = {
-    question: {},
-  };
-
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: {},
+      staticImg: false,
+      img: "",
+    };
+  }
 
 	componentDidMount = () => {
-		this.update();
+		// this.update();
 	}
 	
 	componentDidUpdate = (prevProps) => {
-		if ( 
-      typeof this.props.test.questions[Number(this.props.questionNumber)-1] !== "undefined" && 
-      this.props.test.questions[Number(this.props.questionNumber)-1] !== (typeof prevProps.test.questions !== "undefined" ? prevProps.test.questions[Number(prevProps.questionNumber)-1] : "") 
+    if (
+      typeof prevProps !== "undefined" && 
+      this.props !== prevProps && 
+      this.props.questionNumber !== prevProps.questionNumber
     ) {
-      console.log(this.props.test.questions[Number(this.props.questionNumber)-1])
-      this.setState({question: this.props.test.questions[Number(this.props.questionNumber)-1]})
-			this.update();
-		}
+      // console.log(this.props.test.questions)
+      // this.setState({ 
+      //   question: this.props.test.questions[Number(this.props.questionNumber) - 1]
+      // }, () => this.update());
+      this.update();
+    } 
   }
 
   update = () => {
+    
+    axios
+      .get(apiUrl + "/questions?id=" + this.props.test.questions[Number(this.props.questionNumber) - 1].id, {
+        headers: { Authorization: "Bearer " + Cookies.get("jwt") }
+      }) 
+      .then(response => {
+        // console.log("update");
+        this.setState({ 
+          question: response.data[0]
+        }, function(){
+          console.log(1)
+          if (typeof this.state.question.code_image !== "undefined" && this.state.question.code_image && typeof this.state.question.code_image.url !== "undefined") {
+            this.setState({loading: false});
+            var ext = this.state.question.code_image.url.substring(this.state.question.code_image.url.lastIndexOf(".") + 1);
+            if (ext === "jpg" || ext === "jpeg" || ext === "png" || ext === "gif" || ext === "eps" || ext === "webp") {
+              var imgData = "<img src='" + apiUrl + this.state.question.code_image.url + "'/>";
+              this.setState({
+                staticImg: true,
+                img: imgData,
+                loading: false
+              });
+            } else {
+              axios
+                .get(apiUrl + this.state.question.code_image.url) 
+                .then(response => {
+                  var imgData = response.data;
+                  if (ext !== "svg" && ext !== "html"){
+                    imgData = "<p style='padding: 2rem 0' class='MuiTypography-root MuiTypography-body1 MuiTypography-alignCenter'>This filetype isn't supported.</p>"
+                  }
+                  this.setState({
+                    staticImg: false,
+                    img: imgData,
+                    loading: false
+                  });
+                }).catch(error => { console.log(error) });
+            }
+          } else { 
+            this.setState({ img: "" });
+          }
+        });
+      });
+    // console.log(this.state.question)
+  }
+
+  answer = (answer) => {
+    // axios
+    // .get(apiUrl + "/questions?id=" + this.state.question.id, {
+    //   headers: { Authorization: "Bearer " + Cookies.get("jwt") }
+    // }) 
+    // .then(response => {
+    //   console.log(response)
+    // });
+    // console.log(this.state.question.answers)
+    // console.log([Cookies.get("id")])
+    if (
+      typeof this.state.question.answers !== "undefined" && 
+      this.state.question.answers.some(e => e.user === Number(Cookies.get("id")))
+    ) {
+      axios // Create answer
+        .put(apiUrl + '/answers/' + this.state.question.answers.find(e => e.user === Number(Cookies.get("id"))).id, {
+          true: answer
+        }, { headers: { Authorization: 'Bearer ' + Cookies.get("jwt") } 
+        }).catch(error => { console.log(error); // Handle Error
+        }).then(response => { // Handle success
+          this.update();
+        });
+    } else {
+      axios // Create answer
+        .post(apiUrl + '/answers/', {
+          question: this.state.question.id,
+          user: Cookies.get("id"),
+          true: answer
+        }, { headers: { Authorization: 'Bearer ' + Cookies.get("jwt") } 
+        }).catch(error => { console.log(error); // Handle Error
+        }).then(response => { // Handle success
+          this.update();
+          // axios // Add answer to question
+          // .put(apiUrl + '/answers/', {
+          //   answers: this.state.question.id,
+          //   user: [Cookies.get("id")],
+          //   true: answer
+          // }, { headers: { Authorization: 'Bearer ' + Cookies.get("jwt") } 
+          // }).catch(error => { console.log(error); // Handle Error
+          // }).then(response => { // Handle success
+            
+          // this.props.questionUpdate();
+          // console.log(this.state.question.answers)
+        });
+    }
   }
 
   closeLightbox = () => {
@@ -48,6 +141,17 @@ class AnswerQuestionFields extends React.Component {
     this.setState({lightboxOpen:true})
   };
 
+  nextQuestion = () => {
+    Router.push({
+      pathname: '/answer-question',
+      query: { 
+        test: this.props.test.id, 
+        question: Number(this.props.questionNumber) + 1
+      },
+    }, undefined, { shallow: true });
+    this.props.pageUpdate();
+  }
+
   render() {
     return (
       <>
@@ -55,7 +159,7 @@ class AnswerQuestionFields extends React.Component {
 					<Grid item xs={2}></Grid>
 					<Grid item xs={8}>
             <Box mb={2} mt={2}>
-              <Typography align="left" style={{maxWidth: "700px", marginLeft: "auto", marginRight: "auto"}} variant="h4">{this.state.question.question ? this.state.question.question : ""}</Typography>
+              <Typography align="left" style={{maxWidth: "700px", marginLeft: "auto", marginRight: "auto"}} variant="h4">{typeof this.state.question.question !== "undefined" ? this.state.question.question : ""}</Typography>
             </Box>
 					</Grid>
 					<Grid item xs={2}></Grid>
@@ -63,7 +167,7 @@ class AnswerQuestionFields extends React.Component {
 					<Grid item xs={2}></Grid>
 					<Grid item xs={8}>
 						<Box mb={2}>
-              <Typography align="left" style={{maxWidth: "700px", marginLeft: "auto", marginRight: "auto"}} variant="body1">{this.state.question.context ? this.state.question.context : ""}</Typography>
+              <Typography align="left" style={{maxWidth: "700px", marginLeft: "auto", marginRight: "auto"}} variant="body1">{typeof this.state.question.context !== "undefined"  ? this.state.question.context : ""}</Typography>
 						</Box>
 					</Grid>
 					<Grid item xs={2}></Grid>
@@ -71,41 +175,180 @@ class AnswerQuestionFields extends React.Component {
 
 				<Grid container spacing={4} className="question-fields">
 					<Grid item xs={8}>
-            <Box mb={2}>
-              <Card>
-                <div 
-                  class="question-img-container" 
-                  style={{
-                    width: this.state.question.code_image && this.state.question.code_image.width !== "" ? this.state.question.code_image.width : "inherit", 
-                    height: this.state.question.code_image && this.state.question.code_image.height !== "" ? this.state.question.code_image.height : "inherit"
-                  }}
-                >
-                  {/* { this.state.question.code_image ? 
-                    <CardMedia src={apiUrl + this.state.question.code_image.url} } />
-                  : 
-                    <div className="no-image"></div>
-                  } */}
-                  { this.state.question.code_image ? 
-                    <img src={apiUrl + this.state.question.code_image.url} />
-                  : 
-                    <div className="no-image"></div>
-                  }
-                </div>
-              </Card>
-            </Box>
-					</Grid>
+
+            <Grid container spacing={0}>
+              <Grid item xs={12}>
+                <Box mb={2}>
+                  <Card>
+                    <div 
+                      className="question-img-container" 
+                      style={{
+                        overflow: "scroll"
+                      }}
+                    >
+                      {/* { this.state.question.code_image ? 
+                        <CardMedia src={apiUrl + this.state.question.code_image.url} } />
+                      : 
+                        <div className="no-image"></div>
+                      } */}
+                      {/* { this.state.staticImg ?
+                        <div className="img-visual" dangerouslySetInnerHTML={{__html: this.state.img}} />
+                      :
+                        <iframe id="code-visual" width={this.state.codeData && this.state.codeData.width} height={this.state.codeData && this.state.codeData.height} frameBorder="0" border="0" scrolling="no" srcDoc={this.state.img + "<div id='ext-eqx-styles' style='display:none'></div>"}/>
+                      } */}
+                      { this.state.question.code_data ? 
+                        this.state.staticImg ?
+                          <div className="img-visual" dangerouslySetInnerHTML={{__html: this.state.img}} />
+                        :
+                          <iframe 
+                            id="code-visual" 
+                            width={this.state.question.code_data && this.state.question.code_data.width !== "" ? this.state.question.code_data.width : "inherit"} 
+                            height={this.state.question.code_data && this.state.question.code_data.height !== "" ? this.state.question.code_data.height : "inherit"} 
+                            frameBorder="0" 
+                            border="0" 
+                            scrolling="no" 
+                            srcDoc={this.state.img + "<div id='ext-eqx-styles' style='display:none'></div>"}/>
+                        // <img src={apiUrl + this.state.question.code_image.url} />
+                      : 
+                        <div className="no-image"></div>
+                      }
+                    </div>
+                  </Card>
+                </Box>
+              </Grid>
+              <Grid item xs={12}>
+                <Box mb={6} align="right">
+                  <Button color="primary" size="large" variant="outlined" startIcon={<PermMediaIcon/>}>Show Reference Images</Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
 					<Grid item xs={4}>
 						<Box mb={2}>
-              <ButtonGroup color="primary" fullWidth={true} size="large" aria-label="outlined primary button group">
-                <Button>Yes</Button>
-                <Button>No</Button>
+              <ButtonGroup variant="outlined" color="primary" fullWidth={true} size="large" aria-label="outlined primary button group">
+                <Button 
+                  variant={
+                    typeof this.state.question.answers !== 'undefined' && 
+                    this.state.question.answers.find(e => e.user === Number(Cookies.get("id"))) && 
+                    this.state.question.answers.find(e => e.user === Number(Cookies.get("id"))).true === true 
+                    ? "contained" : "outlined"
+                  } 
+                  onClick={()=>{this.answer(true)}}
+                >Yes</Button>
+                <Button 
+                  variant={
+                    typeof this.state.question.answers !== 'undefined' && 
+                    this.state.question.answers.find(e => e.user === Number(Cookies.get("id"))) && 
+                    this.state.question.answers.find(e => e.user === Number(Cookies.get("id"))).true === false 
+                    ? "contained" : "outlined"
+                  } 
+                  onClick={()=>{this.answer(false)}}
+                >No</Button>
               </ButtonGroup>
             </Box>
             <Box mb={2}>
-              <Button color="primary" fullWidth={true} variant="contained" startIcon={<ArrowForwardIcon/>}>Next Question</Button>
+              <Button onClick={this.nextQuestion} color="primary" disabled={typeof this.state.question.answers !== "undefined" && this.state.question.answers.some(e => e.user === Number(Cookies.get("id"))) ? false : true} fullWidth={true} size="large" variant="contained" startIcon={<ArrowForwardIcon/>}>Next Question</Button>
 						</Box>
             <Box mb={2}>
-              <CommentBox size="small"/>
+              {/* <CommentBox size="small"/> */}
+            { typeof this.state.question.answers !== 'undefined' && 
+              this.state.question.answers.find(e => e.user === Number(Cookies.get("id"))) && 
+              this.state.question.answers.find(e => e.user === Number(Cookies.get("id"))).true === false 
+              ? 
+
+                <Grid container spacing={0}>
+                  <Grid item xs={12}>
+                    <Box mb={2}>
+                      <Divider />
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box mb={2}>
+                      <Button color="primary" fullWidth size="large" variant="outlined" startIcon={<AddCommentIcon/>}>New Comment</Button>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Card className="comment-section" >
+                      <CardHeader
+                        align="left"
+                      // color="primary"
+                      // bgcolor="primary.main"
+                        avatar={
+                          <Avatar aria-label="recipe">
+                            1
+                          </Avatar>
+                        }
+                        title="2 comments"
+                        subheader="Created 10/13/2021"
+                      />
+                      <CardMedia>
+                        <List>
+                          <ListItem>
+                            <ListItemAvatar>
+                              <Avatar alt="Eben Sorkin" src="static/img/eben-avi.png" />
+                            </ListItemAvatar>
+                            <ListItemText
+                              primary="Eben Sorkin"
+                              secondary={
+                                <>
+                                  <Typography
+                                    component="span"
+                                    variant="body2"
+                                    color="textPrimary"
+                                  >
+                                    The contrast is OK but the weight is not a match.
+                                  </Typography>
+                                </>
+                              }
+                            />
+                          </ListItem>
+                          <Divider variant="inset" component="li" />
+                          <ListItem>
+                            <ListItemAvatar>
+                              <Avatar alt="Quinn Keaveney" src="static/img/quinn-avi.png" />
+                            </ListItemAvatar>
+                            <ListItemText
+                              primary="Quinn Keaveney"
+                              secondary={
+                                <>
+                                  <Typography
+                                    component="span"
+                                    variant="body2"
+                                    color="textPrimary"
+                                  >
+                                    I agree, we will have that next week’s test.
+                                  </Typography>
+                                </>
+                              }
+                            />
+                          </ListItem>
+                        </List>
+                      </CardMedia>
+                      <CardActions >
+                        <Box p={1}>
+                          <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                              <TextField
+                                label=""
+                                size="small"
+                                fullWidth
+                                placeholder="Add a reply"
+                                multiline
+                              />
+                            </Grid>
+                            <Grid item xs={12} align="left">
+                              <Button variant="contained" color="primary">Post</Button>
+                              <Button color="primary">Clear</Button>
+                            </Grid>
+                          </Grid>
+                        </Box>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                </Grid>
+              :
+               <></>
+              }
 						</Box>
 					</Grid>
         </Grid>
