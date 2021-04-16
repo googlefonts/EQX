ef930e5c4cb6f648041e90ada83f48b296b9a4fb
QuiteQuinn <quinn@quitetype.com>
2021-04-15 21:07:15 -0700

    Updating footer to reflect relevant repository links

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -8,22 +8,16 @@ const Footer = () => (
     <Container maxWidth={false}>
       <Grid container>
         <Grid item xs={12}>
-          <Typography variant="h5" className="logo" display="inline" style={{fontWeight:700}}>EQX</Typography>
-          <ul style={{display: 'inline-flex'}}>
+          {/* <Typography variant="h5" className="logo" display="inline" style={{fontWeight:700}}>EQX</Typography> */}
+          <ul style={{display: 'inline-flex', padding: "0"}}>
             <li>
-              <Typography variant="body1" gutterBottom><Link href="#"><a>Privacy</a></Link></Typography>
+              <Typography variant="body1" gutterBottom><Link href="https://github.com/SorkinType/EQX"><a>GitHub Repository</a></Link></Typography>
             </li>
             <li>
-              <Typography variant="body1" gutterBottom><Link href="#"><a>Terms</a></Link></Typography>
+              <Typography variant="body1" gutterBottom><Link href="https://github.com/quitequinn/EQX-Visual-Generator"><a>EQX Visual Generator</a></Link></Typography>
             </li>
             <li>
-              <Typography variant="body1" gutterBottom><Link href="#"><a>Contact</a></Link></Typography>
-            </li>
-            <li>
-              <Typography variant="body1" gutterBottom><Link href="#"><a>Developers</a></Link></Typography>
-            </li>
-            <li>
-              <Typography variant="body1" gutterBottom><Link href="#"><a>About</a></Link></Typography>
+              <Typography variant="body1" gutterBottom><Link href="https://github.com/SorkinType/EQX/blob/master/LICENSE"><a>Apache License 2.0</a></Link></Typography>
             </li>
           </ul>
         </Grid>
