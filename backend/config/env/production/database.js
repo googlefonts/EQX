885b5a55e540408264fe369851fb481e930dc0a4
Benji Visser <benny@noqcks.io>
2021-08-31 19:29:43 +0000

    test

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -4,17 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
+        client: 'mysql',
         host: env('DATABASE_HOST', "127.0.0.1"),
-        port: env('DATABASE_PORT', 27017),
+        port: env('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', "strapi"),
         username: env('DATABASE_USERNAME', ""),
         password: env('DATABASE_PASSWORD', ""),
-        schema: 'public'
       },
       options: {
         debug: 'false',
-        ssl: 'false'
+        ssl: 'false',
+        autoMigration: true,
       }
     }
   }
